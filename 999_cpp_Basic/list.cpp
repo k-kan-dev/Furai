6a5fd5f61de4f7e53b25d43cb7a0a0ee297f39ce
k-kan-dev <k.kan.splended@gmail.com>
2025-01-25 14:04:44 +0900

    add: list.cpp

---
+++ 999_cpp_Basic/list.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    
+    int a[10];
+    a[0] = 50;
+
+    int fib[5] = {0, 1, 2, 3, 4};
+
+    int array[50];
+    for (int i = 0; i < 50; i++)
+    {
+        array[i] = 0;
+
+    }
+
+    return 0;
+}
